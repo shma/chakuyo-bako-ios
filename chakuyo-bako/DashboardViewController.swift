be2012494462a011848b60be250bc789425b0044
Matsuno Shunya <shunya.matsuno@townwifi.jp>
2019-04-24 22:07:16 +0900

    first commit

---
+++ chakuyo-bako/DashboardViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DashboardViewController.swift
+//  chakuyo-bako
+//
+//  Created by Matsuno Shunya on 2019/04/24.
+//  Copyright © 2019年 Matsuno Shunya. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class DashboardViewController: UIViewController, BluetoothDelegate {
+
+    let bluetoothManager = CoreBluetoothManager.getInstance()
+
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var presLabel: UILabel!
+    @IBOutlet weak var humLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bluetoothManager.delegate = self
+        bluetoothManager.readEnvironmentData()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func readButtonDidPush(_ sender: Any) {
+        bluetoothManager.readEnvironmentData()
+    }
+    
+    // MARK - BluetoothDelegate
+    func didReadEnvironmentData(tempCal: Double, presCal: Double, humCal: Double) {
+        tempLabel.text = String(tempCal)
+        presLabel.text = String(presCal)
+        humLabel.text = String(humCal)
+    }
+
+}
---
+++ chakuyo-bako/ScanViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  chakuyo-bako
+//
+//  Created by Matsuno Shunya on 2019/04/21.
+//  Copyright © 2019年 Matsuno Shunya. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class ScanViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, BluetoothDelegate  {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var scanButton: UIButton!
+    
+    var peripheralList:[CBPeripheral] = []
+    
+    let bluetoothManager = CoreBluetoothManager.getInstance()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        if bluetoothManager.connectedPeripheral != nil {
+//            bluetoothManager.disconnectPeripheral()
+//        }
+        bluetoothManager.delegate = self
+    }
+
+    @IBAction func scanButtonDidPush(_ sender: Any) {
+        bluetoothManager.scan()
+    }
+    
+    // MARK: Table View Delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peripheralList.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        bluetoothManager.connect(peripheral: peripheralList[indexPath.row])
+    }
+    
+    // MARK: Table View Datasource
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = peripheralList[indexPath.row].name
+        return cell
+    }
+    
+    // MARK: - BluetoothDelegate
+    func didDiscoverPeripheral(_ peripheral: CBPeripheral, advertisementData: [String : Any], RSSI: NSNumber) {
+        peripheralList.append(peripheral)
+        tableView.reloadData()
+    }
+    
+    func didReadValueForCharacteristic(_ characteristic: CBCharacteristic) {
+        let next = storyboard!.instantiateViewController(withIdentifier: "DashboardViewController") as? DashboardViewController
+        self.present(next!,animated: true, completion: { () in
+            // next?.textField2.text = self.textField.text
+        })
+    }
+}
+
